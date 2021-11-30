871f35385491ec816adfee63032d2908ec6856b9
josefdzbeta <jafb19@outlook.com>
2021-12-01 00:19:18 +0100

    contenido

---
+++ analisis/mockups/script.js
@@ -0,0 +1,16 @@
+/**
+ * @Author Jose Angel Betancourt
+ * @description script para tarjeta navideña
+ * @license GPL V3 2021
+ */
+
+'use strict'
+
+console.log('Cargando JS')
+window.onload = iniciar
+
+function iniciar(){
+
+    //let logo = document.getElementById('logoGit')
+
+}
