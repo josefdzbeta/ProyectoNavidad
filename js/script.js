cde23b529cce3199392707aaeae9cc51d703e226
josefdzbeta <jafb19@outlook.com>
2021-12-06 17:33:17 +0100

    shrek

--- js/script.js
+++ js/script.js
@@ -5,6 +5,23 @@
 
 'use strict'
 
+window.onload = iniciar
+
+function iniciar(){
+  document.getElementById('musica').onclick=musicaFondo
+  
+  const MUSICAFONDO=document.querySelector("audio")
+  MUSICAFONDO.loop
+  MUSICAFONDO.volume=0.5
+  function musicaFondo(){
+    if(MUSICAFONDO.paused){
+        MUSICAFONDO.play()
+    }else{
+        MUSICAFONDO.pause()
+    }
+  }
+}
+
 //INTRO
 const intro = document.querySelector('header')
 const text = intro.querySelector("h1")
@@ -22,7 +39,7 @@ let scene = new ScrollMagic.Scene({
 .setPin(intro)
 .addTo(controller)
 
-//Texto
+//Animacion de texto inicial 
 const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
 
 let scene2 = new ScrollMagic.Scene({
@@ -37,13 +54,14 @@ let accelamount = 0.1
 let scrollpos = 0
 let delay = 0
 
+//Convertir en segundos los milisegundos
 scene.on("update", e => {
   scrollpos = e.scrollPos / 1000;
 });
 
+//Conseguir un delay al hacer el scroll para que el delay intente alcanzar la posicion de scroll y así conseguir una transición más suave
 setInterval(() => {
   delay += (scrollpos - delay) * accelamount
-  console.log(scrollpos, delay)
-
+  //console.log(scrollpos, delay)
   video.currentTime = delay
 }, 33.3);
