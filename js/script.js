a54a6f39f1d622e4c6d5a768f493efe3c099ec65
josefdzbeta <jafb19@outlook.com>
2021-12-06 12:50:51 +0100

    librerias y video

--- js/script.js
+++ js/script.js
@@ -7,19 +7,43 @@
 
 //INTRO
 const intro = document.querySelector('header')
-
-//FIN SECCION SCROLL
-const final = document.querySelector('contenedor')
-
+const text = intro.querySelector("h1")
+const video = intro.querySelector("video")
 
 //Scroll
 const controller = new ScrollMagic.Controller()
-
-const escena = new ScrollMagic.Scene({
-    duration: 1000,
-    triggerElemnt: intro,
+//Escenas
+let scene = new ScrollMagic.Scene({
+    duration: 27000,
+    triggerElement: intro,
     triggerHook: 0
 })
-
 .addIndicators()
+.setPin(intro)
 .addTo(controller)
+
+//Texto
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 6000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setTween(textAnim)
+  .addTo(controller);
+//Animacion de video
+let accelamount = 0.1
+let scrollpos = 0
+let delay = 0
+
+scene.on("update", e => {
+  scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount
+  console.log(scrollpos, delay)
+
+  video.currentTime = delay
+}, 33.3);
